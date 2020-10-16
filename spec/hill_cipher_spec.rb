7e1ba3030c94edf43974b91383401300f200b831
LilTits <titouan@guiochet.com>
2020-10-16 07:10:35 +0200

    Spec the encode method of the HillCipher class

--- spec/hill_cipher_spec.rb
+++ spec/hill_cipher_spec.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 RSpec.describe HillCipher do
+  let(:message) { 'act' }
+  let(:key) { 'gybnqkurp' }
+  let(:encrypted_message) { 'poh' }
+  
   describe '#encode' do
-    it 'does something' do
-      expect(1).to eql(1)
+    it 'crypte a message' do
+      expect(subject.encode(message, key)).to eql(encrypted_message)
     end
   end
 end
